10be9fa473b4854e455d1555809efabb8dc701ea
Mohamed Nazim <egynazim@gmail.com>
2018-08-10 16:49:03 -0400

    created routes that render from homepage to url

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -5,6 +5,11 @@ app.get('/', function(req, res){
     res.render('home.ejs');
 });
 
+app.get('/fallinlovewith/:thing', function(req, res){
+    let things = req.params.thing;
+    res.render('love.ejs', {Koo: things});
+});
+
 app.listen(3000, function(){
     console.log('Server is Listening');
 });
